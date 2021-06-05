7978346ac2bfe773cd4f379fe06171cd9743ab72
harry <shsage91@gmail.com>
2021-06-05 19:10:11 +0900

    [ADD] - piano 기능 테스트

--- packages/ui-test/src/index.tsx
+++ packages/ui-test/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 
 
 const root = document.querySelector<HTMLDivElement>("#root")
